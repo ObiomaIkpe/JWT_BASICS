6a64ce97f5c7710a78715c6c68e905a6c4639710
ObiomaIkpe <obiomaikpe1@gmail.com>
2022-10-06 23:15:50 +0100

    complete JWT basics project

---
+++ errors/unauthenticated.js
@@ -0,0 +1,13 @@
+const customAPIError = require('./custom-error')
+const {StatusCodes} = require('http-status-codes')
+
+class unauthenticatedError extends customAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = StatusCodes.UNAUTHORIZED
+      
+    }
+  }
+  
+  module.exports = unauthenticatedError
+  
